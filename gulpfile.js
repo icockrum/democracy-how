ef7278693e76b5089bb525e977b9347cd22afde6
Ivan Cockrum <ivan@weareroyale.com>
2017-02-22 18:57:38 -0800

    dashboard page coming together

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 
 	var gulp  = require( 'gulp' ),
       connect = require('gulp-connect'),
+	    gulpConnectSsi = require('gulp-connect-ssi'),
       del = require('del'),
       gcallback = require('gulp-callback'),
       runSequence = require('run-sequence');
@@ -12,7 +13,17 @@
     return del.sync('www'); 
   }
 	function copy() {
-    return gulp.src(['assets/**/*','./node_modules/uswds/dist/**/*','./node_modules/bootstrap/dist/**/*']).pipe(gulp.dest('www')); 
+    gulp.src([
+    	'assets/**/*',
+    	'./node_modules/uswds/dist/**/*',
+    	'./node_modules/bootstrap/dist/**/*',
+    	])
+    	.pipe(gulp.dest('www')); 
+
+    gulp.src([
+    	'./node_modules/jquery/dist/jquery.min.js'
+    	])
+    	.pipe(gulp.dest('www/js')); 
   }
   function watch() {
     gulp.watch('assets/**/*', ['copy']);
@@ -22,7 +33,15 @@
       port: 9004,
       host: '0.0.0.0',
       root: 'www',
-      livereload: false
+      livereload: false,
+			middleware: function(){
+					return [gulpConnectSsi({
+							baseDir: __dirname + '/www',
+							ext: '.html',
+							domain: 'http://0.0.0.0:9004/',
+							method: 'readOnLineIfNotExist'  // readOnLine|readLocal|readOnLineIfNotExist|downloadIfNotExist 
+					})];
+			}
     });
   }
   function build(callback) {
